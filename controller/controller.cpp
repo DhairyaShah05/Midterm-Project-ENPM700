79a9fdc5ea3ce639fdd255c87dda6d586c726348
DhairyaShah05 <143757600+DhairyaShah05@users.noreply.github.com>
2024-10-29 19:56:03 -0400

    Delete controller/controller.cpp
    Redundant file

--- controller/controller.cpp
+++
@@ -1,9 +0,0 @@
-#include "controller.hpp"
-
-Controller::Controller(Vehicle vehicle)
-    : vehicle_(vehicle), targetHeading_(0.0) {}
-
-void Controller::setTargetHeading(double targetHeading) {
-  targetHeading_ = targetHeading;
-  vehicle_.update(targetHeading_);
-}
