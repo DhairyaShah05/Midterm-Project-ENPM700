e30a2154398d4724f2c540e4dfe4620df4f18fc6
DhairyaShah05 <143757600+DhairyaShah05@users.noreply.github.com>
2024-10-29 20:03:54 -0400

    Update controller.cpp

--- controller/controller.cpp
+++ controller/controller.cpp
@@ -1,1 +1,9 @@
-//Adding for codecoverage
+#include "controller.hpp"
+
+Controller::Controller(Vehicle vehicle)
+    : vehicle_(vehicle), targetHeading_(0.0) {}
+
+void Controller::setTargetHeading(double targetHeading) {
+  targetHeading_ = targetHeading;
+  vehicle_.update(targetHeading_);
+}
